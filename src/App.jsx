5fdcbe534bf283d73edc829f41737238f83bf00b
melonhong <melonhong46@gmail.com>
2025-07-07 01:36:00 +0900

    feat: 리액트 라우터 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Test from "./components/Test/Test";
+import Home from "./components/Home/Home";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
