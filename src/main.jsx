a3f5aa5da7b6fa54b47270d3ba7388af889eadb7
melonhong <melonhong46@gmail.com>
2025-07-08 00:52:25 +0900

    feat: Toolpad router 사용

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,43 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import App from "./App";
+import Layout from "./components/Layout";
+import Home from "./pages/Home/Home";
+import Auth from "./pages/Auth/Auth";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const router = createBrowserRouter([
+  // 라우터 구성
+  {
+    Component: App, // root layout route
+    children: [
+      {
+        // 레이아웃 적용
+        path: "/",
+        Component: Layout,
+        children: [
+          {
+            path: "",
+            Component: Home,
+          },
+        ],
+      },
+      {
+        // 레이아웃 미적용
+        path: "/",
+        children: [
+          {
+            path: "auth",
+            Component: Auth,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
