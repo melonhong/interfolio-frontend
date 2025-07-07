a60c6e21a20fcaf8475e357fde482655a534aba9
melonhong <melonhong46@gmail.com>
2025-07-07 18:52:49 +0900

    feat: Auth 페이지 추가
    - Google로 로그인을 누르면 해당 로그인 페이지로 이동
    - 나머지 소셜 로그인을 누르면 에러가 발생

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Test from "./pages/Test/Test";
 import Home from "./pages/Home/Home";
+import Auth from "./pages/Auth/Auth";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/test" element={<Test />} />
+          <Route path="/auth" element={<Auth />} />
         </Route>
       </Routes>
     </Router>
