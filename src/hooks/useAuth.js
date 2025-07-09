856e86b5c37bfe5d5d4db224de6c07933b7c2350
melonhong <melonhong46@gmail.com>
2025-07-09 15:48:52 +0900

    refactor: 사용하지 않는 파일 삭제

--- src/hooks/useAuth.js
+++
@@ -1,7 +0,0 @@
-// 현재는 간단하게 토큰이 있는지 여부만 따져서 로그인 상태를 확인
-const useAuth = () => {
-  const token = localStorage.getItem("token");
-  return Boolean(token); // true 또는 false 반환
-};
-
-export default useAuth;
